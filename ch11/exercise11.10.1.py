509c1236111819f13b8a7f7b3e9b65d6071ed0b9
Clark <geclark@deloitte.ca>
2019-07-02 06:56:36 -0400

    Chapter 11 Exercise 1 Complete

--- ch11/exercise11.10.1.py
+++ ch11/exercise11.10.1.py
@@ -5,7 +5,15 @@ Write a function that reads the words in words.txt and stores them as keys in a 
 
 If you did Exercise 10, you can compare the speed of this implementation with the list in operator and the bisection search.
 """
-import pandas as pd
+
+## https://www.peterbe.com/plog/how-to-do-performance-micro-benchmarks-in-python
+
+# import pandas as pd
+# import numpy as np
+import search
+import random
+import time
+import statistics
 
 word_dict = {}
 word_list = []
@@ -17,4 +25,27 @@ for line in open("../ch9/words.txt"):
 print("Size of dictionary: {}".format(len(word_dict)))
 print("Size of list: {}".format(len(word_list)))
 
+def f1(word):
+    in_bisect(word_list, word)
 
+def f2(word):
+    word in word_dict
+
+list_times = []
+dict_times = []
+
+for i in range(10000):
+    word = random.choice(word_list)
+
+    t0 = time.time()
+    search.in_bisect(word_list, word)
+    t1 = time.time()
+    list_times.append((t1 - t0) * 1000)
+
+    t0 = time.time()
+    word in word_dict
+    t1 = time.time()
+    dict_times.append((t1 - t0) * 1000)
+
+print('in_bisect used {} times with median: {} and mean: {}'.format(len(list_times), statistics.median(list_times), statistics.mean(list_times)))
+print('in used {} times with median: {} and mean: {}'.format(len(dict_times), statistics.median(dict_times), statistics.mean(dict_times)))
