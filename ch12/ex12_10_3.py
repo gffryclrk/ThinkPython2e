ea7297dd6df33d5196d11df7688d036a015bd2e6
gffryclrk <mr@gffryclrk.com>
2020-01-27 05:17:54 -0500

    Chapter 12 Exercise 3 complete

--- ch12/ex12_10_3.py
+++ ch12/ex12_10_3.py
@@ -5,18 +5,19 @@ Two words form a “metathesis pair” if you can transform one into the other b
 """
 
 import pdb
+import itertools
 
-anagrams = {}
-for line in open("../ch9/words.txt"):
-    word = line.strip()
-    key = tuple(sorted(word))
-    anagram_list = anagrams.get(key, [])
-    anagram_list.append(word)
-    anagrams[key] = anagram_list
-
+def anagram_dict(filename):
+    anagrams = {}
+    for line in open(filename):
+        word = line.strip()
+        key = tuple(sorted(word))
+        anagram_list = anagrams.get(key, [])
+        anagram_list.append(word)
+        anagrams[key] = anagram_list
     
-print("{} anagrams found".format(len(anagrams)))
-
+    return anagrams
+    
 def check_meta_pair(s1, s2):
     """
     This function takes two anagrams and returns a boolean of whether they are metathesis pairs
@@ -31,3 +32,28 @@ def check_meta_pair(s1, s2):
 
 print("Meta pairs converse, conserve: {}".format(check_meta_pair('converse', 'conserve')))
 print("Meta pairs tags, stag: {}".format(check_meta_pair('tags', 'stag')))
+
+"""
+My approach to this puzzle is to use the list of anagrams from previous exercies
+because the metathesis pairs are by definitions anagrams of each other.
+So, looping through the sets of anagrams and for each combination of two, finding out if
+a pair is a metathesis pair
+"""
+
+
+anagrams = anagram_dict("ch9/words.txt") # This file reference, of course, depends on where you're running this script from
+print("{} anagrams found".format(len(anagrams)))
+
+metathesis_pairs = []
+for key in anagrams:
+    anagram_list = anagrams[key]
+    for pair in itertools.combinations(anagram_list, 2):
+        if check_meta_pair(pair[0], pair[1]): metathesis_pairs.append( (pair[0], pair[1]) )
+
+print("{} metathesis pairs found!".format(len(metathesis_pairs)))
+print("Some examples:")
+
+for index, pair in zip(range(100), metathesis_pairs):
+    print(pair, end=' ')
+
+
