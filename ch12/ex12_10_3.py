60565d2f26903d6b80bdd6a0a86031aa36d44e3b
Geoffrey Clark <geclark@deloitte.ca>
2020-01-27 04:51:47 -0500

    Adding Chapter 12 exercise 10.3

---
+++ ch12/ex12_10_3.py
@@ -0,0 +1,33 @@
+"""
+Exercise 3  
+
+Two words form a “metathesis pair” if you can transform one into the other by swapping two letters; for example, “converse” and “conserve”. Write a program that finds all of the metathesis pairs in the dictionary. Hint: don’t test all pairs of words, and don’t test all possible swaps. Solution: http://thinkpython2.com/code/metathesis.py. Credit: This exercise is inspired by an example at http://puzzlers.org.
+"""
+
+import pdb
+
+anagrams = {}
+for line in open("../ch9/words.txt"):
+    word = line.strip()
+    key = tuple(sorted(word))
+    anagram_list = anagrams.get(key, [])
+    anagram_list.append(word)
+    anagrams[key] = anagram_list
+
+    
+print("{} anagrams found".format(len(anagrams)))
+
+def check_meta_pair(s1, s2):
+    """
+    This function takes two anagrams and returns a boolean of whether they are metathesis pairs
+    """
+    swaps = []
+    for t1, t2 in zip(s1, s2):
+        if t1 is not t2:
+            swaps.append( (t1, t2) )
+            if(len(swaps) > 2): return False
+
+    return swaps[0][0] is swaps[1][1] and swaps[0][1] is swaps[1][0]
+
+print("Meta pairs converse, conserve: {}".format(check_meta_pair('converse', 'conserve')))
+print("Meta pairs tags, stag: {}".format(check_meta_pair('tags', 'stag')))
