49d8d651d204849eb7a5b617bb49f5df8d52030a
Geoffrey Clark <geclark@deloitte.ca>
2020-06-09 08:37:07 -0400

    Chapter 13 Exercise 8.8: Implemented choose_suffix() which uses ordered_dictionary of suffixes and their cumulative frequency to choose a suffix in proportion to its appearance in the corpus

--- ch13/ex_13_8_8.py
+++ ch13/ex_13_8_8.py
@@ -25,14 +25,14 @@ First, I want to write an iterator that yeilds n words from a file
 """
 import sys
 sys.path.append('ch13/')
-import ex_13_7_7 as ex13
+import ex_13_7_7 as ex13_7
 
 from cumsum import cumsum
 
 import string
 from collections import deque
 import itertools
-from random import Random
+import random
 import pdb
 
 def read_file(filename, n, skiplines=0):
@@ -98,27 +98,46 @@ def build_organized_dict(dict):
 
     ordered_dict = {}
     for k,v in dict.items():
-        pdb.set_trace()
-        sorted_v_list = ex13.sort_hist_to_list(v)
+        sorted_v_list = ex13_7.sort_hist_to_list(v)
         sorted_freq_list = [x[1] for x in sorted_v_list]
         sorted_word_list = [x[0] for x in sorted_v_list]
         ordered_dict[k] = {'suffixes': sorted_word_list, 'frequencies': cumsum(sorted_freq_list)}
         
 
     return ordered_dict
+def choose_suffix(prefix, ordered_dict):
+    """
+    This function takes a prefix and ordered dictionary and 
+    randomly chooses a suffix with probability in proportion
+    to its number of appearances in the input corpus.
+    """
+
+    v = ordered_dict[prefix]
+    rand_n = random.randint(0, v['frequencies'][-1])
+    rand_word_index = ex13_7.binary_search_leftmost(v['frequencies'], len(v['frequencies']), rand_n)
+    
+    return v['suffixes'][rand_word_index]
+
+
     
 if __name__ == "__main__":
   dict = build_dict(4)
   print("Dict size: {}".format(len(dict)))
 
-  max = 0
-#  for k, v in dict.items():
-#    if len(v) > max:
-#        max = len(v)
-#        print("{}, {}".format(k, v))
 
-#  print("Max number of suffixes for a given prefix: {}".format(max))
 
   ordered_dict = build_organized_dict(dict)
+
+  max = 0
+  for k, v in ordered_dict.items():
+    if v['frequencies'][-1] > max:
+        max = v['frequencies'][-1]
+        print(f"{max}: {k}")
+        print(f"{choose_suffix(k, ordered_dict)}")
+
+    
+
+      
+
   
   
