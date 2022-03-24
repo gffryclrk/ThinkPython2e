53fdb6087f488c94ab327e4265101cc12483e595
Geoffrey Clark <mr@gffryclrk.com>
2022-03-24 06:03:29 -0400

    Successfully import ch13 module to passing unit test
    Also edited the unit test because it was failing due to lack
    of references to self

--- test/test_ch13_12_9.py
+++ test/test_ch13_12_9.py
@@ -3,22 +3,22 @@ Writing some tests for this Zipf thing
 """
 
 import unittest
-import sys
-sys.path.append('ch13/')
-import ex_13_12_9 as zipf
+# import sys
+# sys.path.append('ch13/')
+from ch13 import ex_13_12_9 as zipf
 
 class TestZipfAnalysis(unittest.TestCase):
 
     def setUp(self):
-        emma_frequencies = zipf.build_freq_dict('text/emma.txt')
+        self.emma_frequencies = zipf.build_freq_dict('text/emma.txt')
 
     def test_emma_freqs_greater_than_zero(self):
         """
         Test that the Emma dictionary was loaded
         """
 
-        print(f"Emma Frequencies Dictionary length: {len(emma_frequencies)}")
-        self.assertTrue(len(emma_frequencies) > 0)
+        print(f"Emma Frequencies Dictionary length: {len(self.emma_frequencies)}")
+        self.assertTrue(len(self.emma_frequencies) > 0)
 
         
 if __name__ == '__main__':
