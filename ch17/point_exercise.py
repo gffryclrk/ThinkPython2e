57ded0562b38a10764827e97a7f18d4f0683866a
Geoffrey Clark <mr@gffryclrk.com>
2022-06-15 13:57:17 -0400

    Complete Exercise 17.4: Add method for Point class

--- ch17/point_exercise.py
+++ ch17/point_exercise.py
@@ -11,4 +11,7 @@ and assigns them to the corresponding attributes"""
     def __str__(self):
         return "({}, {})".format(self.x, self.y)
 
+    def __add__(self, other):
+        return Point(self.x + other.x, self.y + other.y)
+
 
--- test/test_ch17_point.py
+++ test/test_ch17_point.py
@@ -23,5 +23,16 @@ class TestObjectOrientedPoint(unittest.TestCase):
         self.assertEqual(self.p.__str__(), '(0, 0)')
         # TODO: Look up how to patch this test to test print without actually printing
 
+    def test_point_add_method(self):
+        """ Exercise 17.4. Write an add method for the Point class. """
+
+        p2 = Point(1,1)
+
+        # TODO: Look up a way to overload equals and use equality to check expected return instead
+        self.assertEqual((self.p + p2).x, 1) 
+        self.assertEqual((self.p + p2).y, 1)
+        
+
+
 if __name__ == '__main__':
     unittest.main()
