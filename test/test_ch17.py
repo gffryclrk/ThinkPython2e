03910f9d42584fe09a7af54b4287b0daf26145b3
Geoffrey Clark <mr@gffryclrk.com>
2022-06-15 13:14:45 -0400

    Implement Ch17 Ex. 17.1 and pass unit test

---
+++ test/test_ch17.py
@@ -0,0 +1,22 @@
+"""Testing exercise solutions to Think Python, 2nd Ed
+Chapter 17 """
+import unittest
+from ch17.time_exercise import Time
+
+
+class TestObjectOrientedTime(unittest.TestCase):
+
+    def setUp(self):
+        self.t = Time()
+        self.t.hour = 1
+        self.t.minute = 0 
+        self.t.second = 0
+
+    def test_oo_time_to_int(self):
+        """ Test object oriented implementation of time_to_int """
+
+        self.assertEqual(
+            self.t.time_to_int(), 3600)
+        
+if __name__ == '__main__':
+    unittest.main()
