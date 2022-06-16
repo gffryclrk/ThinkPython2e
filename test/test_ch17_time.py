5811f8d1543b2e44323a01a20939d6339124f111
Geoffrey Clark <mr@gffryclrk.com>
2022-06-16 05:08:05 -0700

    Complete Exercise 17.6: Time.int_to_time
    I didn't complete this exactly as described by the book. I decided to
    instead modify the `Time.int_to_time` implementation I had written in
    Ch16 and not the one provided in the code sample. To do this I had to
    convert the function to a method and also patch a mistake in the
    unittest `setUp` method. The behaviour now seems to work as expected.

--- test/test_ch17_time.py
+++ test/test_ch17_time.py
@@ -7,16 +7,19 @@ from ch17.time_exercise import Time
 class TestObjectOrientedTime(unittest.TestCase):
 
     def setUp(self):
-        self.t = Time()
-        self.t.hour = 1
-        self.t.minute = 0 
-        self.t.second = 0
+        self.t = Time(1, 0, 0)
 
     def test_oo_time_to_int(self):
         """ Test object oriented implementation of time_to_int """
 
         self.assertEqual(
             self.t.time_to_int(), 3600)
+
+    def test_int_to_time_method(self):
+        """ Confirm Time.int_to_time works as expected """
+
+        self.assertEqual(self.t, Time.int_to_time(3600))
+
         
 if __name__ == '__main__':
     unittest.main()
