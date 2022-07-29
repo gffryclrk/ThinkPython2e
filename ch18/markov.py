a78a470983d9ab72a5ed0ceb2e230dc51a8f2bc0
Geoffrey Clark <mr@gffryclrk.com>
2022-07-29 04:58:37 -0700

    Refactor Markov functions into class

--- ch18/markov.py
+++ ch18/markov.py
@@ -19,93 +19,106 @@ import random
 suffix_map = {}        # map from prefixes to a list of suffixes
 prefix = ()            # current tuple of words
 
-
-def process_file(filename, order=2):
-    """Reads a file and performs Markov analysis.
-
-    filename: string
-    order: integer number of words in the prefix
-
-    returns: map from prefix to list of possible suffixes.
+class Markov:
+    """ As an exercise, download my Markov code from
+    http://thinkpython2.com/code/markov.py, and follow the steps
+    described above to encapsulate the global variables as attributes
+    of a new class called Markov. Solution:
+    http://thinkpython2.com/code/markov2.py.
     """
-    fp = open(filename)
-    skip_gutenberg_header(fp)
 
-    for line in fp:
-        if line.startswith('End of The Project Gutenberg'):
-            break
+    def __init__(self):
 
-        for word in line.rstrip().split():
-            process_word(word, order)
-
-    fp.close()
-
-def skip_gutenberg_header(fp):
-    """Reads from fp until it finds the line that ends the header.
-
-    fp: open file object
-    """
-    for line in fp:
-        if line.startswith('*END*'):
-            break
+        self.suffix_map = {}
+        self.prefix = ()
 
 
-def process_word(word, order=2):
-    """Processes each word.
+    def process_file(self, filename, order=2):
+        """Reads a file and performs Markov analysis.
 
-    word: string
-    order: integer
+        filename: string
+        order: integer number of words in the prefix
 
-    During the first few iterations, all we do is store up the words; 
-    after that we start adding entries to the dictionary.
-    """
-    global prefix
-    if len(prefix) < order:
-        prefix += (word,)
-        return
+        returns: map from prefix to list of possible suffixes.
+        """
+        fp = open(filename)
+        self.skip_gutenberg_header(fp)
 
-    try:
-        suffix_map[prefix].append(word)
-    except KeyError:
-        # if there is no entry for this prefix, make one
-        suffix_map[prefix] = [word]
+        for line in fp:
+            if line.startswith('End of The Project Gutenberg'):
+                break
 
-    prefix = shift(prefix, word)
+            for word in line.rstrip().split():
+                self.process_word(word, order)
+
+        fp.close()
+
+    def skip_gutenberg_header(self, fp):
+        """Reads from fp until it finds the line that ends the header.
+
+        fp: open file object
+        """
+        for line in fp:
+            if line.startswith('*END*'):
+                break
 
 
-def random_text(n=100):
-    """Generates random wordsfrom the analyzed text.
+    def process_word(self, word, order=2):
+        """Processes each word.
 
-    Starts with a random prefix from the dictionary.
+        word: string
+        order: integer
 
-    n: number of words to generate
-    """
-    # choose a random prefix (not weighted by frequency)
-    start = random.choice(list(suffix_map.keys()))
-    
-    for i in range(n):
-        suffixes = suffix_map.get(start, None)
-        if suffixes == None:
-            # if the start isn't in map, we got to the end of the
-            # original text, so we have to start again.
-            random_text(n-i)
+        During the first few iterations, all we do is store up the words;
+        after that we start adding entries to the dictionary.
+        """
+
+        if len(self.prefix) < order:
+            self.prefix += (word,)
             return
 
-        # choose a random suffix
-        word = random.choice(suffixes)
-        print(word, end=' ')
-        start = shift(start, word)
+        try:
+            self.suffix_map[self.prefix].append(word)
+        except KeyError:
+            # if there is no entry for this prefix, make one
+            self.suffix_map[self.prefix] = [word]
+
+        prefix = self.shift(self.prefix, word)
 
 
-def shift(t, word):
-    """Forms a new tuple by removing the head and adding word to the tail.
+    def random_text(self, n=100):
+        """Generates random wordsfrom the analyzed text.
 
-    t: tuple of strings
-    word: string
+        Starts with a random prefix from the dictionary.
 
-    Returns: tuple of strings
-    """
-    return t[1:] + (word,)
+        n: number of words to generate
+        """
+        # choose a random prefix (not weighted by frequency)
+        start = random.choice(list(self.suffix_map.keys()))
+
+        for i in range(n):
+            suffixes = self.suffix_map.get(start, None)
+            if suffixes == None:
+                # if the start isn't in map, we got to the end of the
+                # original text, so we have to start again.
+                self.random_text(n-i)
+                return
+
+            # choose a random suffix
+            word = random.choice(suffixes)
+            print(word, end=' ')
+            start = self.shift(start, word)
+
+
+    def shift(self, t, word):
+        """Forms a new tuple by removing the head and adding word to the tail.
+
+        t: tuple of strings
+        word: string
+
+        Returns: tuple of strings
+        """
+        return t[1:] + (word,)
 
 
 def main(script, filename='158-0.txt', n=100, order=2):
@@ -115,8 +128,9 @@ def main(script, filename='158-0.txt', n=100, order=2):
     except ValueError:
         print('Usage: %d filename [# of words] [prefix length]' % script)
     else: 
-        process_file(filename, order)
-        random_text(n)
+        m = Markov()
+        m.process_file(filename, order)
+        m.random_text(n)
         print()
 
 
